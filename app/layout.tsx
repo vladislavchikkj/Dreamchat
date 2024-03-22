66ae09a3746c1c8b951e718453d8860ae6f3ea45
Vlad <vlad.kosevich@mail.ru>
2024-03-22 21:11:16 +0300

    back to next 13

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import ActiveStatus from "./components/ActiveStatus";
+import AuthContext from "./context/AuthContext";
+import ToasterContext from "./context/ToasterContext";
+import "./globals.css";
+
+export const metadata = {
+  title: "DreamChat",
+  description: "Dreamchat",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
+}
